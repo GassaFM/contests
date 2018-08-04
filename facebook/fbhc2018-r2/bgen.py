2d512bf37d44e5ac9ee3f638d91dc0031f61bcbd
Ivan Kazmenko <gassafm@gmail.com>
2018-08-05 00:16:04 +0300

    fbhc 2018 r2: +generators for B

---
+++ facebook/fbhc2018-r2/bgen.py
@@ -0,0 +1,13 @@
+import random
+random.seed (26236)
+
+t = 20
+print t
+for tt in range (t):
+	n = 200000
+	m = 1000000
+	a = random.randrange (n)
+	b = random.randrange (n)
+	print n, m, a, b
+	for i in range (n - 1):
+		print random.randrange (i + 1)
